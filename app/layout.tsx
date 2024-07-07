72c3243c9b62424005a3385e0f770e70797a88ce
Matias De la Vega <mattias.delavega92@gmail.com>
2024-07-07 19:48:07 -0300

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google';
+import { inter } from './ui/invoices/fonts';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
